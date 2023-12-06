843637507ab9036d5b49e88483930e733f21acca
alimdfaiz <144793353+alimdfaiz@users.noreply.github.com>
2023-12-06 15:08:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateText(){
+	let paragraph = document.getElementById("status");
+	paragraph.innerHTML = "<h1>Entered Metaverse</h1>";
+}
+
+
+
+
+// let para = document.querySelector(#status);
+// let btn =  document.querySelector(#enterBtn);
+// para.innerHtml = <h1>Entered Metaverse</h1>;
+// btn.innerText = Entered Metaverse ;
+// console.log(btn.innerText);
